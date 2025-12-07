554199105722e3b5e806d26e37c6926b17562ab3
BodhiHarper <bodhi.harper@icloud.com>
2025-12-07 15:31:33 -0800

    Update LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -197,10 +197,10 @@ $wgGroupPermissions['observers']['viewsuppressed'] = true;
 # Namespace aliases
 $wgNamespaceAliases['KT'] = NS_PROJECT;
 
-# Remove the limitations of only supporting images
+# Remove the limitations of only supporting images as well as adding .svg format support
 $wgFileExtensions = array_merge(
     $wgFileExtensions,
-    ['ogg', 'oga', 'ogv', 'mp3', 'wav', 'flac', 'opus', 'webm']
+    ['ogg', 'oga', 'ogv', 'mp3', 'wav', 'flac', 'opus', 'webm', 'svg']
 );
 wfLoadExtension( 'TimedMediaHandler' );
 
