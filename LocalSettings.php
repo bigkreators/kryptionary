bf0cb20be72ab2e4ccaead9468e0cffdb2c239f5
BodhiHarper <bodhi.harper@icloud.com>
2025-12-28 12:33:40 -0800

    Update LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -132,7 +132,7 @@ $wgDiff3 = "";
 
 ## Default skin: you can change the default skin. Use the internal symbolic
 ## names, e.g. 'vector' or 'monobook':
-$wgDefaultSkin = "timeless";
+$wgDefaultSkin = "vector-2022";
 
 # Enabled skins.
 # The following skins were automatically enabled:
